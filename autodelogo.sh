cd1698f55cff6b9593c959cffa31523a0fe6fea2
Your Name <you@example.com>
2022-08-08 21:44:42 +0800

    deploy

---
+++ autodelogo.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+wget -O download.mp4 $1
+
+echo ------------------spliting------------------------
+#00:00:00
+ffmpeg -i download.mp4 -vcodec copy -acodec copy -t $TIME tmp1.mp4
+ffmpeg -i download.mp4 -vcodec copy -acodec copy -ss $TIME tmp2.mp4
+
+rm -rf download.mp4
+
+echo ------------------delogoing------------------------
+# "delogo=x=432:y=44:w=1060:h=108"
+ffmpeg -i tmp1.mp4 -vf $POSITION -c:a copy tmp3.mp4
+
+echo ------------------forcetbn-------------------------
+ffmpeg -i tmp3.mp4 -strict -2 -video_track_timescale $TBN tmp4.mp4
+
+echo ------------------merging------------------------
+echo file tmp4.mp4 > mylist.txt && echo file tmp2.mp4 >> mylist.txt
+ffmpeg -f concat -i mylist.txt -c copy output.mp4
+
+rm -rf tmp* mylist.txt
+
+mkdir main
+
+echo ------------------converting------------------------
+ffmpeg -i output.mp4 -codec: copy -start_number 0 -hls_time 10 -hls_list_size 0 -f hls ./main/main.m3u8
+
+rm -rf output.mp4
+
+# mv main upload
+
+# cd /content/drive/MyDrive/upload && cp main.go /content/upload
+
+# echo ------------------uploading------------------------
+# cd /content/upload && go mod init upload && go mod tidy && go run main.go main
+
+# cd /content && rm -rf upload
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+        "os"
+        "github.com/web3-storage/go-w3s-client"
+        "fmt"
+        "context"
+        "database/sql"
+
+        _ "github.com/lib/pq"
+)
+
+func main(){
+
+
+        index := os.Getenv("INDEX")
+
+        urls := strings.Split(os.Getenv("URLS"), ";")
+
+        for i, v := range urls{
+
+                cmd := os.Command("chmod", "+x", "autodelogo.sh")
+                cmd.Run()
+
+                cmd = os.Command("bash", "autodelogo.sh", v)
+                cmd.Run()
+
+                c, _ := w3s.NewClient(w3s.WithToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDMyQWI1NThkQWVCN2Y5MjQ3NzY5ZTM3MGZkYTBGYTNFNmRlM2I2QWMiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NDMxMzIwNDY1MTgsIm5hbWUiOiJzdG9yYWdlIn0.-sEIB2KQ48wP0GeCx53hUKvEqPJ7wFw7Qf1yseY8kUs"))
+
+
+                dir, err := os.Open("main")
+                if err!= nil{
+                        panic(err)
+                }
+                cid, err := c.Put(context.Background(), dir)
+                if err != nil{
+                        panic(err)
+                }
+
+                cmd = os.Command("rm", "-rf", "main")
+                cmd.Run()
+
+                db, err := sql.Open("postgres", `postgres://evaddaucvcbnxo:785c7b60fead46d306ace829c26b00d815ebf12d053f37fb00626fc01945e9e1@ec2-54-75-26-218.eu-west-1.compute.amazonaws.com:5432/d58pvsk1dskehn`)
+                if err != nil{
+                        log.Fatal(err)
+                }
+
+                if _, err := db.Exec(`INSERT INTO detail_table(index, episode_index, episode_url) VALUES($1, $2, $3)`, index, i+1, fmt.Sprintf("%v", cid)); err != nil{
+                        log.Fatal(err)
+                }
+        }
+}
