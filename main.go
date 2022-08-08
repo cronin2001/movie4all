d7aa897cfcb0a79d3498d29fbd5b702f7f578376
berge2001 <110832401+berge2001@users.noreply.github.com>
2022-08-08 13:56:29 +0000

    Update main.go

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
         "os"
+        "os/exec"
         "github.com/web3-storage/go-w3s-client"
         "fmt"
         "context"
         "database/sql"
+        "strings"
+        "log"
 
         _ "github.com/lib/pq"
 )
@@ -19,10 +22,10 @@ func main(){
 
         for i, v := range urls{
 
-                cmd := os.Command("chmod", "+x", "autodelogo.sh")
+                cmd := exec.Command("chmod", "+x", "autodelogo.sh")
                 cmd.Run()
 
-                cmd = os.Command("bash", "autodelogo.sh", v)
+                cmd = exec.Command("bash", "autodelogo.sh", v)
                 cmd.Run()
 
                 c, _ := w3s.NewClient(w3s.WithToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDMyQWI1NThkQWVCN2Y5MjQ3NzY5ZTM3MGZkYTBGYTNFNmRlM2I2QWMiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NDMxMzIwNDY1MTgsIm5hbWUiOiJzdG9yYWdlIn0.-sEIB2KQ48wP0GeCx53hUKvEqPJ7wFw7Qf1yseY8kUs"))
@@ -37,7 +40,7 @@ func main(){
                         panic(err)
                 }
 
-                cmd = os.Command("rm", "-rf", "main")
+                cmd = exec.Command("rm", "-rf", "main")
                 cmd.Run()
 
                 db, err := sql.Open("postgres", `postgres://evaddaucvcbnxo:785c7b60fead46d306ace829c26b00d815ebf12d053f37fb00626fc01945e9e1@ec2-54-75-26-218.eu-west-1.compute.amazonaws.com:5432/d58pvsk1dskehn`)
