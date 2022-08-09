b76d83b5761dac40afe46972d75ee54031654b23
berge2001 <110832401+berge2001@users.noreply.github.com>
2022-08-09 12:04:48 +0800

    Update main.go

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
         "strings"
         "log"
         "time"
+        "io/ioutil"
+        "io"
 
         _ "github.com/lib/pq"
 )
@@ -26,17 +28,42 @@ func main(){
         episode_index = strings.Split(os.Getenv("EPISODE_INDEX"), ",");
         urls = strings.Split(os.Getenv("URLS"), ";")
 
+
         for i, v := range urls{
 
                 go func(i int, v string){
-                        cmd := exec.Command("chmod", "+x", "autodelogo.sh")
+
+                        cmd := exec.Command("wget", "-O", "download.mp4", v)
+                        stdout, err := cmd.StdoutPipe()
+                        if err != nil{
+                                log.Fatal(err)
+                        }
+                        go func(){
+                                reader := bufio.NewReader(stdout)
+                                for{
+                                        line, err := r.ReadString("\n")
+                                        if err != nil || err == io.EOF{
+                                                return
+                                        }
+                                        fmt.Println(line)
+                                }
+                        }()
+                        cmd.Run()
+
+                        cmd = exec.Command("chmod", "+x", "autodelogo.sh")
                         cmd.Run()
 
                         cmd = exec.Command("bash", "autodelogo.sh", v)
                         cmd.Run()
 
-                        c, _ := w3s.NewClient(w3s.WithToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDMyQWI1NThkQWVCN2Y5MjQ3NzY5ZTM3MGZkYTBGYTNFNmRlM2I2QWMiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NDMxMzIwNDY1MTgsIm5hbWUiOiJzdG9yYWdlIn0.-sEIB2KQ48wP0GeCx53hUKvEqPJ7wFw7Qf1yseY8kUs"))
 
+                        dirmain, _ := ioutil.ReadDir("main")
+                        if len(dir) == 0{
+                                continue
+                        }
+
+
+                        c, _ := w3s.NewClient(w3s.WithToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDMyQWI1NThkQWVCN2Y5MjQ3NzY5ZTM3MGZkYTBGYTNFNmRlM2I2QWMiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NDMxMzIwNDY1MTgsIm5hbWUiOiJzdG9yYWdlIn0.-sEIB2KQ48wP0GeCx53hUKvEqPJ7wFw7Qf1yseY8kUs"))
 
                         dir, err := os.Open("main")
                         if err!= nil{
