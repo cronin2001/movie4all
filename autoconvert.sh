174f89fb7600fa52171dd49ae57579c64fad18a7
berge2001 <110832401+berge2001@users.noreply.github.com>
2022-08-10 19:16:41 +0800

    Update and rename autodelogo.sh to autoconvert.sh

--- autoconvert.sh
+++ autoconvert.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 
-echo ------------------spliting------------------------
-#00:00:00
-ffmpeg -i download.mp4 -vcodec copy -acodec copy -t $TIME tmp1.mp4
-ffmpeg -i download.mp4 -vcodec copy -acodec copy -ss $TIME tmp2.mp4
-
-rm -rf download.mp4
-
-echo ------------------delogoing------------------------
-# "delogo=x=432:y=44:w=1060:h=108"
-ffmpeg -i tmp1.mp4 -vf $POSITION -c:a copy tmp3.mp4
-
 echo ------------------forcetbn-------------------------
-ffmpeg -i tmp2.mp4 -strict -2 -video_track_timescale $1 tmp4.mp4
-ffmpeg -i tmp3.mp4 -strict -2 -video_track_timescale $1 tmp5.mp4
+ffmpeg -i tmp3.mp4 -strict -2 -video_track_timescale $1 tmp4.mp4
 
 echo ------------------merging------------------------
-echo file tmp5.mp4 > mylist.txt && echo file tmp4.mp4 >> mylist.txt
+echo file tmp4.mp4 > mylist.txt && echo file tmp2.mp4 >> mylist.txt
 ffmpeg -f concat -i mylist.txt -c copy output.mp4
 
 rm -rf tmp* mylist.txt
