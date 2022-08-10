65c26ba1337c789eaa90c9ee55481d79494c1bfe
berge2001 <110832401+berge2001@users.noreply.github.com>
2022-08-10 19:15:36 +0800

    Create autodelogo1.sh

---
+++ autodelogo1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo ------------------spliting------------------------
+#00:00:00
+ffmpeg -i download.mp4 -vcodec copy -acodec copy -t $TIME tmp1.mp4
+ffmpeg -i download.mp4 -vcodec copy -acodec copy -ss $TIME tmp2.mp4
+
+rm -rf download.mp4
+
+echo ------------------delogoing------------------------
+# "delogo=x=432:y=44:w=1060:h=108"
+ffmpeg -i tmp1.mp4 -vf $POSITION -c:a copy tmp3.mp4
