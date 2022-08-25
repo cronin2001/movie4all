2187e64f90660f30bf1b044ccdad2ff70658e4e2
cronin2001 <111212715+cronin2001@users.noreply.github.com>
2022-08-26 00:29:39 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,48 +1,49 @@
 package main
 
 import (
-    "fmt"
-    "regexp"
-    "encoding/json"
-    "log"
-    "net/http"
-    "io/ioutil"
-    "os"
-    "os/exec"
-    "github.com/web3-storage/go-w3s-client"
-    "context"
-    "database/sql"
-    "strings"
-    "time"
-    "strconv"
-    "gopkg.in/vansante/go-ffprobe.v2"
+	"context"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/web3-storage/go-w3s-client"
+	"gopkg.in/vansante/go-ffprobe.v2"
 
 	_ "github.com/lib/pq"
 )
 
-
-type response struct{
-
-        URL string `json:"url"`
-        ID string `json:"id"`
-        FROM string `json:"from"`
-        URL_NEXT string `json:"url_next"`
-        NID int `json:"nid"`
+type response struct {
+	URL      string `json:"url"`
+	ID       string `json:"id"`
+	FROM     string `json:"from"`
+	URL_NEXT string `json:"url_next"`
+	NID      int    `json:"nid"`
 }
 
-
-func findsubmatch(rege string, body string)[]string{
+func findsubmatch(rege string, body string) []string {
 
 	reg := regexp.MustCompile(rege)
 	return reg.FindStringSubmatch(body)
 }
 
 var (
-	res response
+	res      response
 	videourl string
+	start    string
+	latest   bool = false
+	failed   []string
 )
 
-func gettbn()(string, error){
+func gettbn() (string, error) {
 	ctx, cancelFn := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancelFn()
 
@@ -56,7 +57,7 @@ func gettbn()(string, error){
 		log.Printf("Error getting data: %v", err)
 	}
 
-	if data == nil{
+	if data == nil {
 		return "", fmt.Errorf("empty response")
 	}
 
@@ -64,179 +65,217 @@ func gettbn()(string, error){
 	return tbn[1], nil
 }
 
-func deferfunc(){
+func deferfunc() {
 
 	cmd := exec.Command("rm", "-rf", "main")
 	cmd.Run()
 }
 
-func handle(url string){
-    cmd := exec.Command("chmod", "+x", "autodelogo.sh")
-    cmd.Run()
-    cmd = exec.Command("chmod", "+x", "autoconvert.sh")
-    cmd.Run()
+func handle(url string) {
+	cmd := exec.Command("chmod", "+x", "autodelogo.sh")
+	cmd.Run()
+	cmd = exec.Command("chmod", "+x", "autoconvert.sh")
+	cmd.Run()
 
-    log.Printf("downloading: %s\n", url);
-    cmd = exec.Command("wget", "--timeout=30", "-O", "download.mp4", url)
-    cmd.Run()
-
+	log.Printf("downloading: %s\n", url)
+	cmd = exec.Command("wget", "--timeout=30", "-O", "download.mp4", url)
+	cmd.Run()
 
 	var position string
-	
-	if size := strings.Contains(url, "540P"); size{
+
+	if size := strings.Contains(url, "540P"); size {
 		position = "delogo=x=64:y=2:w=560:h=54"
-	}else if size := strings.Contains(url, "720P"); size{
+	} else if size := strings.Contains(url, "720P"); size {
 		position = "delogo=x=64:y=2:w=650:h=80"
-	}else{
+	} else {
 		position = "delogo=x=380:y=20:w=1100:h=225"
 	}
 
-		
-    cmd = exec.Command("bash", "autodelogo.sh", position)
-    cmd.Run()
+	cmd = exec.Command("bash", "autodelogo.sh", position)
+	cmd.Run()
 
-    tbn, err := gettbn()
-    if err != nil{
-        return
-    }
-    log.Printf("the current tbn is: %s", tbn)
-		
-    cmd = exec.Command("bash", "autoconvert.sh", tbn)
-    cmd.Run()
-		
+	tbn, err := gettbn()
+	if err != nil {
+		failed = append(failed, url)
+		return
+	}
+	log.Printf("the current tbn is: %s", tbn)
 
-    dirmain, _ := ioutil.ReadDir("main")
-    if len(dirmain) == 0{
-        log.Println("the folder's empty")
-        deferfunc()
-        return
-    }
+	cmd = exec.Command("bash", "autoconvert.sh", tbn)
+	cmd.Run()
 
+	dirmain, _ := ioutil.ReadDir("main")
+	if len(dirmain) == 0 {
+		log.Println("the folder's empty")
+		deferfunc()
+		return
+	}
 
-    c, _ := w3s.NewClient(w3s.WithToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDMyQWI1NThkQWVCN2Y5MjQ3NzY5ZTM3MGZkYTBGYTNFNmRlM2I2QWMiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NDMxMzIwNDY1MTgsIm5hbWUiOiJzdG9yYWdlIn0.-sEIB2KQ48wP0GeCx53hUKvEqPJ7wFw7Qf1yseY8kUs"))
+	c, _ := w3s.NewClient(w3s.WithToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDMyQWI1NThkQWVCN2Y5MjQ3NzY5ZTM3MGZkYTBGYTNFNmRlM2I2QWMiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NDMxMzIwNDY1MTgsIm5hbWUiOiJzdG9yYWdlIn0.-sEIB2KQ48wP0GeCx53hUKvEqPJ7wFw7Qf1yseY8kUs"))
 
-    dir, err := os.Open("main")
-    if err!= nil{
-        deferfunc()
-        return
-    }
-    cid, err := c.Put(context.Background(), dir)
-    if err != nil{
-        deferfunc()
-        return
-    }
+	dir, err := os.Open("main")
+	if err != nil {
+		deferfunc()
+		return
+	}
+	cid, err := c.Put(context.Background(), dir)
+	if err != nil {
+		deferfunc()
+		failed = append(failed, url)
+		return
+	}
 
-    db, err := sql.Open("postgres", `postgres://evaddaucvcbnxo:785c7b60fead46d306ace829c26b00d815ebf12d053f37fb00626fc01945e9e1@ec2-54-75-26-218.eu-west-1.compute.amazonaws.com:5432/d58pvsk1dskehn`)
-    if err != nil{
-        deferfunc()
-        return
-    }
-    defer db.Close()
+	db, err := sql.Open("postgres", `postgres://evaddaucvcbnxo:785c7b60fead46d306ace829c26b00d815ebf12d053f37fb00626fc01945e9e1@ec2-54-75-26-218.eu-west-1.compute.amazonaws.com:5432/d58pvsk1dskehn`)
+	if err != nil {
+		deferfunc()
+		failed = append(failed, url)
+		return
+	}
+	defer db.Close()
 
-    if _, err := db.Exec(`INSERT INTO detail_table(index, episode_index, episode_url) VALUES($1, $2, $3)`, res.ID, res.NID, fmt.Sprintf("%v", cid)); err != nil{
-        deferfunc()
-        return
-    }
+	if _, err := db.Exec(`INSERT INTO detail_table(index, episode_index, episode_url) VALUES($1, $2, $3)`, res.ID, res.NID, fmt.Sprintf("%v", cid)); err != nil {
+		deferfunc()
+		failed = append(failed, url)
+		return
+	}
 }
 
-func main(){
+func dispatch(url string) {
 
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
 
-	count, _ := strconv.Atoi(os.Getenv("COUNT"))
-	start := os.Getenv("START")
+	req, _ := http.NewRequest("GET", url, nil)
 
-	for{
+	req.Host = "zxzj.vip"
 
-		client := &http.Client{
-			Timeout: 10*time.Second,
-		}
+	resp, err := client.Do(req)
+	if err != nil {
+		failed = append(failed, url)
+		return
+	}
+	defer resp.Body.Close()
 
-		req, _ := http.NewRequest("GET", start+fmt.Sprintf("%v", count)+`.html`, nil)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		failed = append(failed, url)
+		return
+	}
 
-		req.Host = "zxzj.vip"
+	result := findsubmatch(`(?is)player_aaaa=(.*?)<`, string(body))
 
-		resp , err := client.Do(req)
-		if err != nil{
-				log.Fatal(err)
+	if len(result) < 2 {
+		failed = append(failed, url)
+		return
+	}
+
+	if err := json.Unmarshal([]byte(result[1]), &res); err != nil {
+		failed = append(failed, url)
+		return
+	} else {
+		fmt.Printf("%+v\n", res)
+	}
+
+	if res.URL == "" {
+		failed = append(failed, url)
+		return
+	}
+
+	if res.FROM == "dpp" {
+		req, _ = http.NewRequest("GET", "https://jx.zxzj.vip/dplayer.php?url="+res.URL, nil)
+
+		req.Host = "jx.zxzj.vip"
+
+		resp, err = client.Do(req)
+		if err != nil {
+			failed = append(failed, url)
+			return
 		}
 		defer resp.Body.Close()
 
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil{
-				log.Fatal(err)
+		body, err = ioutil.ReadAll(resp.Body)
+		if err != nil {
+			failed = append(failed, url)
+			return
 		}
 
-		result := findsubmatch(`(?is)player_aaaa=(.*?)<`, string(body))
-
-		if len(result) < 2{
-				log.Fatal("no more element")
+		result = findsubmatch(`(?is)var urls = '(.*?)';`, string(body))
+		if len(result) < 2 {
+			failed = append(failed, url)
+			return
 		}
 
-		if err := json.Unmarshal([]byte(result[1]), &res); err != nil{
-				log.Fatal(err)
-		}else{
-				fmt.Printf("%+v\n", res)
+		videourl = result[1]
+	} else if res.FROM == "ck" {
+		req, _ = http.NewRequest("GET", "https://jx.zxzj.vip/ckplayer.php?url="+res.URL, nil)
+
+		req.Host = "jx.zxzj.vip"
+
+		resp, err = client.Do(req)
+		if err != nil {
+			failed = append(failed, url)
+			return
 		}
-		
-		if res.URL == ""{
-			continue
-		}
+		defer resp.Body.Close()
 
-		if res.FROM == "dpp"{
-			req, _ = http.NewRequest("GET", "https://jx.zxzj.vip/dplayer.php?url="+res.URL, nil)
-
-			req.Host = "jx.zxzj.vip"
-
-			resp , err = client.Do(req)
-			if err != nil{
-				log.Fatal(err)
-			}
-			defer resp.Body.Close()
-
-			body, err = ioutil.ReadAll(resp.Body)
-			if err != nil{
-					log.Fatal(err)
-			}
-
-			result = findsubmatch(`(?is)var urls = '(.*?)';`, string(body))
-			if len(result) < 2{
-				log.Fatal("no more element")
-			}
-
-			videourl = result[1]
-		}else if res.FROM == "ck"{
-			req, _ = http.NewRequest("GET", "https://jx.zxzj.vip/ckplayer.php?url="+res.URL, nil)
-
-			req.Host = "jx.zxzj.vip"
-
-			resp , err = client.Do(req)
-			if err != nil{
-				log.Fatal(err)
-			}
-			defer resp.Body.Close()
-
-			body, err = ioutil.ReadAll(resp.Body)
-			if err != nil{
-					log.Fatal(err)
-			}
-
-			result = findsubmatch(`(?is)var urls = '(.*?)';`, string(body))
-			if len(result) < 2{
-				log.Fatal("no more element")
-			}
-
-			videourl = result[1]
+		body, err = ioutil.ReadAll(resp.Body)
+		if err != nil {
+			failed = append(failed, url)
+			return
 		}
 
-		//视频下载处理部分
-		handle(videourl)
-		
-		//最后一集退出
-		if res.URL_NEXT == "G"{
-			break
+		result = findsubmatch(`(?is)var urls = '(.*?)';`, string(body))
+		if len(result) < 2 {
+			failed = append(failed, url)
+			return
 		}
 
-		count++
+		videourl = result[1]
+	}
+
+	//视频下载处理部分
+	handle(videourl)
+
+	//最后一集退出
+	if res.URL_NEXT == "G" {
+		latest = true
+	}
+}
+
+func main() {
+
+	start = os.Getenv("START")
+	mode := os.Getenv("MODE")
+
+	//判断是全部还是部分
+	if mode == `all` {
+
+		count, _ := strconv.Atoi(os.Getenv("COUNT"))
+
+		for {
+			if !latest {
+				dispatch(start + fmt.Sprintf("%v", count) + `.html`)
+				count++
+			} else {
+				break
+			}
+		}
+	} else if mode == `partly` {
+
+		lists := os.Getenv("LISTS")
+		list := strings.Split(lists, ",")
+
+		for _, v := range list {
+			dispatch(start + fmt.Sprintf("%v", v) + `.html`)
+		}
+	} else {
+		return
+	}
+
+	//再次尝试失败
+	for _, v := range failed {
+		dispatch(v)
 	}
 
 }
